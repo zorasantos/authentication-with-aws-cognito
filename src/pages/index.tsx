ee578dcbfb3269c02a0c71661f47cf83caf99da7
zorasantos <zoranildo22@gmail.com>
2022-06-05 09:29:24 -0300

    Restructured project and config tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Login with aws cognito
+    </h1>
+  )
+}
+
+export default Home
